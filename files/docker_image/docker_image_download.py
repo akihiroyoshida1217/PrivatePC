22b744aab4b795e73f7492159493a1ac6477a201
akihiroyoshida1217 <akihiro.takahashi.1217@gmail.com>
2020-03-27 17:52:31 +0900

    corrected docker image script

--- files/docker_image/docker_image_download.py
+++ files/docker_image/docker_image_download.py
@@ -5,81 +5,91 @@ import requests
 import json
 import os
 import tarfile
+import subprocess
 
-image_name = 'debian'
-tag = 'latest'
+token_url = 'https://auth.docker.io/token'
+request_url = 'https://registry-1.docker.io/v2/library/'
 
-os.makedirs(image_name)
+def layerdownload(image_name, headers, digest, config_response):
+        layer_path = image_name + '/' + digest.replace('sha256:','')
+        
+        os.makedirs(layer_path)
 
-params = (
-    ('service', 'registry.docker.io'),
-    ('scope', 'repository:library/' + image_name + ':pull'),
-)
+        blob_response = requests.get(request_url + image_name + '/blobs/' + digest , headers=headers)
 
-token_response = requests.get('https://auth.docker.io/token', params=params)
-token = json.dumps(token_response.json()['token']).strip("\"")
+        blob_json = {
+            "id": digest.replace('sha256:',''),
+            "created": config_response.json().get("created"),
+            "container": config_response.json().get("container"),
+            "container_config": config_response.json().get("container_config"),
+            "docker_version": config_response.json().get("docker_version"),
+            "config": config_response.json().get("config"),
+            "architecture": config_response.json().get("architecture"),
+            "os": config_response.json().get("os")
+          }
+        #print(json.dumps(blob_json))
 
-#print(token)
+        with open(layer_path + '/layer.tar', 'wb') as saveFile, \
+             open(layer_path + '/json', 'w') as saveJSON, \
+             open(layer_path + '/VERSION', 'w') as saveVERSION:
+                saveFile.write(blob_response.content)
+                json.dump(blob_json, saveJSON)
+                saveVERSION.write('1.0')
 
-headers = {
-    'Authorization': 'Bearer ' + token,
-    'Accept': 'application/vnd.docker.distribution.manifest.v2+json'
-}
+def imagedownload(image_name = 'debian', tag = 'latest'):
+        os.makedirs(image_name)
 
-manifest_response = requests.get('https://registry-1.docker.io/v2/library/' + image_name + '/manifests/' + tag, headers=headers)
-#print(json.dumps(manifest_response.json()['layers'][0]['digest']))
+        params = (
+                ('service', 'registry.docker.io'),
+                ('scope', 'repository:library/' + image_name + ':pull'),
+        )
 
-config_digest = json.dumps(manifest_response.json()['config']['digest']).strip("\"")
-config_response = requests.get('https://registry-1.docker.io/v2/library/' + image_name + '/blobs/' + config_digest , headers=headers)
-#print(config_response.text)
-with open(image_name + '/' + config_digest.replace('sha256:','') + '.json', 'w') as saveCONFIG:
-        saveCONFIG.write(config_response.text)
+        token_response = requests.get(token_url, params=params)
+        token = json.dumps(token_response.json()['token']).strip("\"")
+        #print(token)
 
-manifest_json = [{
-    "config": config_digest.replace('sha256:','') + ".json",
-    "RepoTags": [
-      image_name + ":" + tag
-    ],
-    "Layers": [ d['digest'].replace('sha256:','') + '/layer.tar' for d in manifest_response.json()['layers'] ]
-  }]
-#print(json.dumps(manifest_json))
-with open(image_name + '/' + 'manifest.json', 'w') as saveMANIFEST:
-        json.dump(manifest_json, saveMANIFEST)
+        headers = {
+                'Authorization': 'Bearer ' + token,
+                'Accept': 'application/vnd.docker.distribution.manifest.v2+json'
+        }
 
-repositories_json = {
-    image_name : {
-      tag : manifest_response.json()['layers'][-1]['digest'].replace('sha256:','')
-    }
-  }
-#print(json.dumps(repositories_json))
-with open(image_name + '/' + 'repositories', 'w') as saveREPOSITORIES:
-        json.dump(repositories_json, saveREPOSITORIES)
+        manifest_response = requests.get(request_url + image_name + '/manifests/' + tag, headers=headers)
+        #print(json.dumps(manifest_response.json()['layers'][0]['digest']))
 
-digest = json.dumps(manifest_response.json()['layers'][0]['digest']).strip("\"")
+        config_digest = json.dumps(manifest_response.json()['config']['digest']).strip("\"")
+        config_response = requests.get(request_url + image_name + '/blobs/' + config_digest , headers=headers)
+        #print(config_response.text)
 
-os.makedirs(image_name + '/' + digest.replace('sha256:',''))
+        repositories_json = {
+            image_name : {
+              tag : manifest_response.json()['layers'][-1]['digest'].replace('sha256:','')
+            }
+          }
+        #print(json.dumps(repositories_json))
 
-blob_response = requests.get('https://registry-1.docker.io/v2/library/' + image_name + '/blobs/' + digest , headers=headers)
-with open(image_name + '/' + digest.replace('sha256:','') + '/layer.tar', 'wb') as saveFile:
-        saveFile.write(blob_response.content)
+        manifest_json = [{
+            "config": config_digest.replace('sha256:','') + ".json",
+            "RepoTags": [
+              image_name + ":" + tag
+            ],
+            "Layers": [ d['digest'].replace('sha256:','') + '/layer.tar' for d in manifest_response.json()['layers'] ]
+          }]
+        #print(json.dumps(manifest_json))
 
-blob_json = {
-    "id": digest.replace('sha256:',''),
-    "created": config_response.json().get("created"),
-    "container": config_response.json().get("container"),
-    "container_config": config_response.json().get("container_config"),
-    "docker_version": config_response.json().get("docker_version"),
-    "config": config_response.json().get("config"),
-    "architecture": config_response.json().get("architecture"),
-    "os": config_response.json().get("os")
-  }
+        with open(image_name + '/' + config_digest.replace('sha256:','') + '.json', 'w') as saveCONFIG, \
+             open(image_name + '/' + 'manifest.json', 'w') as saveMANIFEST, \
+             open(image_name + '/' + 'repositories', 'w') as saveREPOSITORIES:
+                saveCONFIG.write(config_response.text)
+                json.dump(manifest_json, saveMANIFEST)
+                json.dump(repositories_json, saveREPOSITORIES)
 
-#print(json.dumps(blob_json))
-with open(image_name + '/' + digest.replace('sha256:','') + '/json', 'w') as saveJSON:
-        json.dump(blob_json, saveJSON)
+        #digest = json.dumps(manifest_response.json()['layers'][0]['digest']).strip("\"")
 
-with open(image_name + '/' + digest.replace('sha256:','') + '/VERSION', 'w') as saveVERSION:
-        saveVERSION.write('1.0')
+        [layerdownload(image_name, headers, json.dumps(layer['digest']).strip("\""), config_response) for layer in manifest_response.json()['layers']]
 
-with tarfile.open(image_name + '.tar', 'w') as tar:
-        tar.add(image_name + '/')
+        #with tarfile.open(image_name + '.tar', 'w') as tar:
+        #        tar.add(image_name + '/')
+        subprocess.call(["tar", "cvf", image_name + ".tar", "-C", image_name, "."])
+
+if __name__ == "__main__":
+        imagedownload()
